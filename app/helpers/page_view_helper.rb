067e44ee3d5d545dd9726c0da95e40a39e99618b
Orian Dabud <orian@opinya.co.il>
2015-05-13 14:37:55 +0300

    added manual controller

---
+++ app/helpers/page_view_helper.rb
@@ -0,0 +1,2 @@
+module PageViewHelper
+end
---
+++ test/controllers/recommandation_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RecommandationControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
