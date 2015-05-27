cc2a72b528439838cc99ad53f3b3cea318792346
Orian Dabud <orian@opinya.co.il>
2015-05-27 12:32:21 +0300

    categories logic

--- app/controllers/page_view_controller.rb
+++ app/controllers/page_view_controller.rb
@@ -1,22 +1,21 @@
 class PageViewController < ApplicationController
   require 'open-uri'
   skip_before_filter :verify_authenticity_token
-  before_action :set_product , only: [:create ]
+  before_action :set_products , only: [:create ]
   before_action :set_user , only: [:create ]
   before_action :set_popularity , only: [:create ]
 
   # POST /websites
   # POST /websites.json
   def create
-    @popularity.update_attribute( 'entrances', @popularity.entrances+1) if @popularity
-
     render nothing: true, status: 204
   end
 
   private
 
-  def set_product
-    @product = Product.find_by_page_link(params[:url]) || Product.create_using_crawler(params[:url] , Website.find_by_url(params[:website]))
+  def set_products
+    @products = [Product.find_by_page_link(params[:url])] ||
+                Factory::ViewAble.new(params[:url] , Website.find_by_name(params[:website]))
   end
 
   def set_user
@@ -24,6 +23,9 @@ class PageViewController < ApplicationController
   end
 
   def set_popularity
-    @popularity = @product.popularities.where(user: @user).first || Popularity.create(user: @user , product: @product , entrances: 0) if @product
+    @products.each do |product|
+      popularity = product.popularities.where(user: @user).first || Popularity.create(user: @user , product: product , entrances: 0)
+      popularity.update_attribute( 'entrances', popularity.entrances+1)
+    end
   end
 end
---
+++ lib/factory.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+# For testing and manual email sending
+
+module Factory
+
+  class ViewAble
+
+    def initialize(url , website)
+      doc = Nokogiri::HTML(open(url).read)
+
+      if doc.at_css(website.product_page.validator_selector)
+
+        title        = doc.at_css(website.product_page.name_selector).text
+        picture_link = doc.at_css(website.product_page.picture_selector).xpath('@src').first.value
+        price        = doc.at_css(website.product_page.price_selector).text.scan(/[\d\.]/).join('')
+
+        [Product.create(page_link: url,picture_link: picture_link, title: title , price: price , website: website)]
+
+
+      elsif doc.at_css(website.category_page.validator_selector)
+
+        Category.create(page_link: url, website: website)
+
+        products_urls = doc.at_css(website.category_page.products_selector).xpath('@href')
+
+        products = []
+        products_urls.each do |product_url|
+          products += ViewAble.new(product_url,website)
+        end
+        products
+
+      else
+
+        []
+      end
+
+    end
+
+    def self.find_by_page_link url
+      Product.find_by_page_link(url) ||
+      Category.find_by_page_link(url)
+    end
+
+  end
+
+end
